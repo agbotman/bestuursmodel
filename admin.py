d7b7360633b4ecdd86352ee32afec7fe4e901214
Ton Botman <agbotman@yahoo.com>
2013-02-25 15:52:54 +0100

    FunctieInline toevoegen aan TaakAdmin

--- admin.py
+++ admin.py
@@ -15,8 +15,16 @@ class FunctieAdmin(admin.ModelAdmin):
         FunctieEisInline,
     ]
 
+class FunctieInline(admin.TabularInline):
+    model = Functie.taken.through
+
+class TaakAdmin(admin.ModelAdmin):
+    inlines = [
+        FunctieInline,
+    ]
+
 admin.site.register(Afdeling, AfdelingAdmin)
-admin.site.register(Taak)
+admin.site.register(Taak, TaakAdmin)
 admin.site.register(Verantwoordelijkheid)
 admin.site.register(Functie, FunctieAdmin)
 admin.site.register(Sector)
