87eee5c1b5ef0da3a06cf40cfdc5119966d75382
Ton Botman <agbotman@yahoo.com>
2013-02-25 15:02:33 +0100

    FunctieEisInline toevoegen aan FunctieAdmin

--- admin.py
+++ admin.py
@@ -7,10 +7,18 @@ class AfdelingAdmin(admin.ModelAdmin):
         models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':40})},
     }
 
+class FunctieEisInline(admin.TabularInline):
+    model = FunctieEis.functies.through
+
+class FunctieAdmin(admin.ModelAdmin):
+    inlines = [
+        FunctieEisInline,
+    ]
+
 admin.site.register(Afdeling, AfdelingAdmin)
 admin.site.register(Taak)
 admin.site.register(Verantwoordelijkheid)
-admin.site.register(Functie)
+admin.site.register(Functie, FunctieAdmin)
 admin.site.register(Sector)
 admin.site.register(FunctieEis)
 admin.site.register(AfdelingFunctie)
