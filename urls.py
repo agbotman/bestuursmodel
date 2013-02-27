39319fd9da67457b36c34bd873e587b4d35dd5c9
Ton Botman <agbotman@yahoo.com>
2013-02-27 12:56:46 +0100

    Functie template and view toegevoegd

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('bestuursmodel.views',
-    (r'^bestuursmodel/$', 'afdelingoverzicht'),
+    (r'^bestuursmodel/$', 'afdeling'),
     (r'^bestuursmodel/afdeling/$', 'afdeling'),
     (r'^bestuursmodel/afdeling/(\w+)/$', 'afdeling'),
+    (r'^bestuursmodel/functie/$', 'functie'),
+    (r'^bestuursmodel/functie/(\w+)/$', 'functie'),
 )
--- views.py
+++ views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .models import *
 
-def afdelingoverzicht(request):
-    return render_to_response('afdelingoverzicht.html')
-
 def afdeling(request, afdelingsid=1):
     try:
         afdeling = Afdeling.objects.get(id=int(afdelingsid))
@@ -13,4 +10,14 @@ def afdeling(request, afdelingsid=1):
     afdelingen = Afdeling.objects.all()
     return render_to_response('afdeling.html',
                               {'afdeling': afdeling, 'afdelingen': afdelingen},
+                              RequestContext(request))
+
+def functie(request, functieid=1):
+    try:
+        functie = Functie.objects.get(id=int(functieid))
+    except:
+        functie = Functie.objects.get(id=1)
+    functies = Functie.objects.all()
+    return render_to_response('functie.html',
+                              {'functie': functie, 'functies': functies},
                               RequestContext(request))
