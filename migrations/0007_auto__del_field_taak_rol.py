904320786d498d0544782597370d87dcf33a7e47
Ton Botman <agbotman@yahoo.com>
2013-03-05 15:24:47 +0100

    correctie

--- migrations/0007_auto__del_field_taak_rol.py
+++ migrations/0007_auto__del_field_taak_rol.py
@@ -8,7 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting field 'Taak.rol'
+        # Deleting z field 'Taak.rol'
         db.delete_column('bestuursmodel_taak', 'rol_id')
 
 
