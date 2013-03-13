72669815aa22acab4b6c0ab26d97cbef54411698
Ton Botman <agbotman@yahoo.com>
2013-03-13 16:17:14 +0100

    Admin aanpassingen voor M2M relation Taak - Rol

--- admin.py
+++ admin.py
@@ -15,46 +15,44 @@ class FunctieAdmin(admin.ModelAdmin):
     inlines = [
         FunctieEisInline,
     ]
-
-class RolForm(ModelForm):
-    class Meta:
-        model = Rol
-
-    taken = ModelMultipleChoiceField(queryset=Taak.objects.all())
-
-    def __init__(self, *args, **kwargs):
-        super(RolForm, self).__init__(*args, **kwargs)
-        if self.instance:
-            self.fields['taken'].initial = self.instance.taken.all()
-
-    def save(self, *args, **kwargs):
-        instance = super(RolForm, self).save(commit=False)
-        self.fields['taken'].initial.update(rol=None)
-        self.cleaned_data['taken'].update(rol=instance)
-        return instance
-
-class RolAdmin(admin.ModelAdmin):
-    form = RolForm
-
-class TaakForm(ModelForm):
-    class Meta:
-        model = Taak
-
-    rollen = ModelMultipleChoiceField(queryset=Rol.objects.all())
-
-    def __init__(self, *args, **kwargs):
-        super(TaakForm, self).__init__(*args, **kwargs)
-        if self.instance:
-            self.fields['rollen'].initial = self.instance.rollen.all()
-
-    def save(self, *args, **kwargs):
-        instance = super(TaakForm, self).save(commit=False)
-        self.fields['rollen'].initial.update(taak=None)
-        self.cleaned_data['rollen'].update(taak=instance)
-        return instance
+    
+class RolInline(admin.TabularInline):
+    model = Rol.taken.through
 
 class TaakAdmin(admin.ModelAdmin):
-    form = TaakForm
+    inlines = [
+        RolInline,
+    ]
+    
+class TaakInline(admin.TabularInline):
+    model = Taak.rollen.through
+
+class RolAdmin(admin.ModelAdmin):
+    inlines = [
+        TaakInline,
+    ]
+    
+
+#class RolForm(ModelForm):
+#    class Meta:
+#        model = Rol
+#
+#    taken = ModelMultipleChoiceField(queryset=Taak.objects.all())
+#
+#    def __init__(self, *args, **kwargs):
+#        super(RolForm, self).__init__(*args, **kwargs)
+#        if self.instance:
+#            self.fields['taken'].initial = self.instance.taken.all()
+#
+#    def save(self, *args, **kwargs):
+#        instance = super(RolForm, self).save(commit=False)
+#        self.fields['taken'].initial.update(rol=None)
+#        self.cleaned_data['taken'].update(rol=instance)
+#        return instance
+#
+#class RolAdmin(admin.ModelAdmin):
+#    form = RolForm
+
 
 admin.site.register(Afdeling, AfdelingAdmin)
 admin.site.register(Taak, TaakAdmin)
